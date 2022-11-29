631db11b8304902dde8bc0d4532d1d7b422485e7
= <=>
2022-11-30 00:15:44 +0200

    remove route

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ Route::get('/', function () {
     return view('posts');
 });
 
-Route::get('/', function () {
-    return view('posts');
-});
+
 
 Route::get('/posts/{post}', function ($slug) {
     
